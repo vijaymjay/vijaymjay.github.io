fba94ca7edbf76a82c5b1d862a8430bb6c74e690
Vijay <vijay@wisdomtoolz.com>
2024-01-24 00:24:58 +0530

    Fixed issues

---
+++ Assets/js/script.js
@@ -0,0 +1,21 @@
+
+document.getElementsByClassName("tablinks")[0].className += " active";
+
+document.getElementsByClassName("tabcontent")[1].style.display = "none";
+
+function openCity(evt, cityName) {
+
+  
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "flex";
+  evt.currentTarget.className += " active";
+}
+
