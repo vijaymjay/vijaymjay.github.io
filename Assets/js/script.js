2c3bb0657e6c1400071df64015261fc8b82abfb9
Vijay <vijay@wisdomtoolz.com>
2024-02-05 23:35:58 +0530

    updated changes

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,23 +1,5 @@
 
-document.getElementsByClassName("tablinks")[0].className += " active";
 
-document.getElementsByClassName("tabcontent")[1].style.display = "none";
-
-function openCity(evt, cityName) {
-
-  
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(cityName).style.display = "flex";
-  evt.currentTarget.className += " active";
-}
 
 // const starClassActive = "rating__star fas fa-star";
 // const starClassInactive = "rating__star far fa-star";
@@ -44,26 +26,7 @@ function openCity(evt, cityName) {
 
 // executeRating(allstars);
 
-const ratingStars = [...document.getElementsByClassName("rating__star")];
 
-function executeRating(stars) {
-  const starClassActive = "rating__star fas fa-star";
-  const starClassInactive = "rating__star far fa-star";
-  const starsLength = stars.length;
-  let i;
-  stars.map((star) => {
-    star.onclick = () => {
-      i = stars.indexOf(star);
-
-      if (star.className === starClassInactive) {
-        for (i; i >= 0; --i) stars[i].className = starClassActive;
-      } else {
-        for (i; i < starsLength; ++i) stars[i].className = starClassInactive;
-      }
-    };
-  });
-}
-executeRating(ratingStars);
 
 function gotoCourseDetail(){
   document.location.href='coursedetail.html';
@@ -89,10 +52,7 @@ else {
  
 }
 
-
-
-
-  
+ 
  }
 
 
